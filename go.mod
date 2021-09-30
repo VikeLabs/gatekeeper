ae098af855f20e33c695db83b33afeb9aef06ec1
Malcolm Seyd <malcolmseyd@gmail.com>
2021-09-29 18:41:34 -0700

    start experimenting with slash commands

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go v1.35.17
 	github.com/diamondburned/arikawa v1.3.14
+	github.com/diamondburned/arikawa/v3 v3.0.0-rc.2 // indirect
 	github.com/go-redis/redis/v8 v8.4.2
 	github.com/guregu/dynamo v1.10.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 )
--- main.go
+++ main.go
@@ -1,41 +1,255 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/diamondburned/arikawa/bot"
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/diamondburned/arikawa/v3/state"
+	"github.com/diamondburned/arikawa/v3/utils/json/option"
 )
 
-// To run, do `BOT_TOKEN="TOKEN HERE" go run .`
+// To run, do `APP_ID="APP ID" GUILD_ID="GUILD ID" BOT_TOKEN="TOKEN HERE" go run .`
 
 func main() {
-	var token = os.Getenv("BOT_TOKEN")
+	appID := discord.AppID(mustSnowflakeEnv("APP_ID"))
+	guildID := discord.GuildID(mustSnowflakeEnv("GUILD_ID"))
+
+	token := os.Getenv("BOT_TOKEN")
 	if token == "" {
 		log.Fatalln("No $BOT_TOKEN given.")
 	}
 
-	wait, err := bot.Start(token, &struct{ Ctx *bot.Context }{}, func(ctx *bot.Context) error {
-		ctx.HasPrefix = bot.NewPrefix("!", "~")
-		ctx.EditableCommands = true
+	s, err := state.New("Bot " + token)
+	if err != nil {
+		log.Fatalln("Session failed:", err)
+		return
+	}
 
-		// Subcommand demo, but this can be in another package.
-		// ctx.MustRegisterSubcommand(&Debug{})
+	s.AddHandler(func(e *gateway.InteractionCreateEvent) {
+		switch e.Data.Type() {
+		case discord.CommandInteraction:
+			cmd := e.Data.(*discord.CommandInteractionData)
+			name := cmd.Name
+			options := cmd.Options
 
-		return nil
+			var data api.InteractionResponse
+			switch name {
+			case "ping":
+				latency := time.Since(e.ID.Time())
+				response := "Pong! `time=" + latency.String() + "`"
+
+				data = api.InteractionResponse{
+					Type: api.MessageInteractionWithSource,
+					Data: &api.InteractionResponseData{
+						Content: option.NewNullableString(response),
+						Flags:   api.EphemeralResponse,
+					},
+				}
+			case "echo":
+				var echoedMsg string
+				for _, v := range options {
+					if v.Name == "message" {
+						echoedMsg = v.String()
+					}
+				}
+				thisGuild, err := s.Guild(e.GuildID)
+				if err != nil {
+					log.Println("wtf! guild doesn't exist")
+					return
+				}
+
+				ownerID := thisGuild.OwnerID
+				senderID := e.Member.User.ID
+				if ownerID != senderID {
+					echoedMsg = "Sorry, only the server owner can use this command."
+				}
+
+				data = api.InteractionResponse{
+					Type: api.MessageInteractionWithSource,
+					Data: &api.InteractionResponseData{
+						Content: option.NewNullableString(echoedMsg),
+						Flags:   api.EphemeralResponse,
+					},
+				}
+			}
+
+			if err := s.RespondInteraction(e.ID, e.Token, data); err != nil {
+				log.Println("failed to send interaction callback:", err)
+			}
+
+		case discord.PingInteraction:
+			data := api.InteractionResponse{
+				Type: api.PongInteraction,
+			}
+			if err := s.RespondInteraction(e.ID, e.Token, data); err != nil {
+				log.Println("failed to send interaction callback:", err)
+			}
+		}
 	})
 
+	s.AddIntents(gateway.IntentGuilds)
+
+	if err := s.Open(context.Background()); err != nil {
+		log.Fatalln("failed to open:", err)
+	}
+	defer s.Close()
+
+	log.Println("Gateway connected. Getting all guild commands.")
+
+	commands, err := s.GuildCommands(appID, guildID)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("failed to get guild commands:", err)
 	}
 
-	log.Println("Bot started")
+	for _, command := range commands {
+		log.Println("Existing command", command.Name, "found.")
 
-	// As of this commit, wait() will block until SIGINT or fatal. The past
-	// versions close on call, but this one will block.
-	// If for some reason you want the Cancel() function, manually make a new
-	// context.
-	if err := wait(); err != nil {
-		log.Fatalln("Gateway fatal error:", err)
+		// delete pre-existing commands for this bot (in case a cleanup failed or something)
+		if command.AppID == appID {
+			s.DeleteGuildCommand(appID, command.GuildID, command.ID)
+		}
 	}
+
+	newCommands := []api.CreateCommandData{
+		{
+			Name:        "register",
+			Description: "Join the server by registering your email",
+			Type:        discord.ChatInputCommand,
+			Options: []discord.CommandOption{
+				{
+					Name:        "email",
+					Description: "The email that you'd like to register",
+					Type:        discord.StringOption,
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "verify",
+			Description: "Finalize registration by verifying your email",
+			Type:        discord.ChatInputCommand,
+			Options: []discord.CommandOption{
+				{
+					Name:        "token",
+					Description: "The that you recieved in your email",
+					Type:        discord.StringOption,
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:                "whois",
+			Description:         "Admin only: get the user's indentifier",
+			Type:                discord.ChatInputCommand,
+			NoDefaultPermission: true,
+			Options: []discord.CommandOption{
+				{
+					Name:        "user",
+					Description: "The user whos identifier will be given",
+					Type:        discord.UserOption,
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:                "setup",
+			Description:         "Initialize the bot with necessary info to run",
+			Type:                discord.ChatInputCommand,
+			NoDefaultPermission: true,
+			Options: []discord.CommandOption{
+				{
+					Name:        "domain",
+					Description: "A domain to be allowlisted",
+					Type:        discord.StringOption,
+					Required:    true,
+				},
+				{
+					Name:        "verified_role",
+					Description: "The role that will be assigned to verified users",
+					Type:        discord.RoleOption,
+					Required:    true,
+				},
+				{
+					Name:        "verification_channel",
+					Description: "The channel that verification will occur in",
+					Type:        discord.ChannelOption,
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "echo",
+			Description: "Just like Unix",
+			Type:        discord.ChatInputCommand,
+			Options: []discord.CommandOption{
+				{
+					Name:        "message",
+					Description: "Echo me!",
+					Type:        discord.StringOption,
+					Required:    true,
+				},
+			},
+		},
+	}
+
+	// track commands so we can delete them on cleanup
+	activeCommands := []*discord.Command{}
+
+	for _, command := range newCommands {
+		newCmd, err := s.CreateGuildCommand(appID, guildID, command)
+		if err != nil {
+			log.Fatalln("failed to create guild command:", err)
+		}
+		activeCommands = append(activeCommands, newCmd)
+
+		if command.NoDefaultPermission {
+			s.EditCommandPermissions(appID, guildID, newCmd.ID, []discord.CommandPermissions{
+				// {
+				// 	ID:
+				// }
+			})
+		}
+	}
+
+	wait := func() func() {
+		sigChan := make(chan os.Signal)
+
+		signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+		return func() {
+			<-sigChan
+		}
+	}()
+
+	// block until ctrl+c or panic
+	fmt.Println("blocking...")
+	wait()
+	fmt.Print("\n")
+	fmt.Println("cleaning up")
+
+	// cleanup
+	for _, cmd := range activeCommands {
+		err := s.DeleteGuildCommand(cmd.AppID, cmd.GuildID, cmd.ID)
+		if err != nil {
+			log.Println("couldn't delete command with id", cmd)
+		}
+	}
+
+	fmt.Println("exiting")
+}
+
+func mustSnowflakeEnv(env string) discord.Snowflake {
+	s, err := discord.ParseSnowflake(os.Getenv(env))
+	if err != nil {
+		log.Fatalf("Invalid snowflake for $%s: %v", env, err)
+	}
+	return s
 }
